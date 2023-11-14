b7e2077e8ffdfb5fc3d88afb296fd35c6faefc24
Michael Gabriel <134394030+Mikeabah@users.noreply.github.com>
2023-11-14 23:50:10 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
+
+var a = console.log.bind(document);
+a(3 * 4);
+a("Hello!");
+a(true);
